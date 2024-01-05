3f672b7f95a83564aaad2a3adf6b4ffb871fd7c7
yasuyaswa <83386305+yasuyaswa@users.noreply.github.com>
2024-01-05 14:54:18 +0530

    Update NoPrefixSet.cs

--- Week4/NoPrefixSet.cs
+++ Week4/NoPrefixSet.cs
@@ -1,6 +1,80 @@
 using System;
 using System.Collections.Generic;
-
+using System.IO;
+ 
+class Trie
+{
+    private const int LETTER_SIZE = 'j' - 'a' + 1;
+ 
+    class Node
+    {
+        public Node[] Children;
+        public char Key;
+        public int WordCount = 0;
+        public int PrefixCount = 0;
+ 
+        public Node(char key)
+        {
+            Key = key;
+            Children = new Node[LETTER_SIZE];
+        }
+ 
+        public bool Has(char key)
+        {
+            return Get(key) != null;
+        }
+ 
+        public Node Get(char key)
+        {
+            return Children[GetKey(key)];
+        }
+ 
+        public void Put(char key, Node node)
+        {
+            Children[GetKey(key)] = node;
+        }
+ 
+        public char GetKey(char ch)
+        {
+            return (char)(ch - 'a');
+        }
+    }
+ 
+    private Node Root = new Node('*');
+ 
+    public bool Insert(string word)
+    {
+        return Insert(word, Root);
+    }
+ 
+    private bool Insert(string word, Node parent)
+    {
+        parent.PrefixCount++;
+        if (word.Length >= 0 && parent.WordCount > 0)
+        {
+            return false;
+        }
+        if (word.Length == 0)
+        {
+            if (parent.PrefixCount > 1)
+            {
+                return false;
+            }
+            parent.WordCount++;
+            return true;
+        }
+ 
+        char ch = word[0];
+        Node next = parent.Get(ch);
+        if (next == null)
+        {
+            next = new Node(ch);
+            parent.Put(ch, next);
+        }
+        return Insert(word.Substring(1), next);
+    }
+}
+ 
 class Result
 {
     /*
@@ -8,70 +82,43 @@ class Result
      *
      * The function accepts STRING_ARRAY words as parameter.
      */
-
-    public static void noPrefix(List<string> words)
+ 
+    public static void NoPrefix(List<string> words)
     {
-        TrieNode root = new TrieNode();
-
-        foreach (var word in words)
+        bool good = true;
+        Trie trie = new Trie();
+        foreach (string word in words)
         {
-            if (!InsertWord(root, word))
+            good = trie.Insert(word);
+            if (!good)
             {
                 Console.WriteLine("BAD SET");
                 Console.WriteLine(word);
-                return;
+                break;
             }
         }
-
-        Console.WriteLine("GOOD SET");
-    }
-
-    private static bool InsertWord(TrieNode root, string word)
-    {
-        TrieNode current = root;
-
-        for (int i = 0; i < word.Length; i++)
+ 
+        if (good)
         {
-            char ch = word[i];
-
-            if (current.Children[ch - 'a'] == null)
-            {
-                current.Children[ch - 'a'] = new TrieNode();
-            }
-            else if (current.Children[ch - 'a'].IsEnd)
-            {
-                return false;  // Word is a prefix of another word
-            }
-
-            current = current.Children[ch - 'a'];
+            Console.WriteLine("GOOD SET");
         }
-
-        current.IsEnd = true;
-
-        return true;
     }
 }
-
-class TrieNode
-{
-    public TrieNode[] Children { get; } = new TrieNode[26];
-    public bool IsEnd { get; set; }
-}
-
+ 
 class Solution
 {
     public static void Main(string[] args)
     {
         int n = Convert.ToInt32(Console.ReadLine().Trim());
-
+ 
         List<string> words = new List<string>();
-
+ 
         for (int i = 0; i < n; i++)
         {
             string wordsItem = Console.ReadLine();
             words.Add(wordsItem);
         }
-
-        Result.noPrefix(words);
+ 
+        Result.NoPrefix(words);
     }
 }
